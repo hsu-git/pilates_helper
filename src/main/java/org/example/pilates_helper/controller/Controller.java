54955a0387706f38e69cb5209204e2a0410f7beb
hsu-git <hsu080206@gmail.com>
2025-03-12 10:38:36 +0900

    구조

---
+++ src/main/java/org/example/pilates_helper/controller/Controller.java
@@ -0,0 +1,8 @@
+package org.example.pilates_helper.controller;
+
+import jakarta.servlet.http.HttpServlet;
+
+public abstract class Controller extends HttpServlet
+{
+
+}
---
+++ src/main/java/org/example/pilates_helper/service/TogetherService.java
@@ -0,0 +1,6 @@
+package org.example.pilates_helper.service;
+
+public class TogetherService
+{
+
+}
