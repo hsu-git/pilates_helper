55934b6710054e25cb0718aa08eac60ecb16583c
hsu-git <hsu080206@gmail.com>
2025-03-12 14:43:12 +0900

    baseModel

--- src/main/java/org/example/pilates_helper/service/TogetherService.java
+++ src/main/java/org/example/pilates_helper/service/TogetherService.java
@@ -2,6 +2,7 @@ package org.example.pilates_helper.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.pilates_helper.model.dto.BaseLLMResponse;
 import org.example.pilates_helper.model.dto.ModelType;
 import org.example.pilates_helper.model.dto.TogetherAPIParam;
 import org.example.pilates_helper.model.repository.TogetherRepository;
@@ -16,10 +17,11 @@ public class TogetherService {
     }
 
     public String useBaseModel(String prompt) throws JsonProcessingException {
-        return repository.callAPI(new TogetherAPIParam(
+        String responseText = repository.callAPI(new TogetherAPIParam(
                 prompt,
                 ModelType.BASE
         ));
+        return objectMapper.readValue(responseText, BaseLLMResponse.class).choices().get(0).message().content();
     }
 
     public String useReasoning(String prompt) throws JsonProcessingException {
